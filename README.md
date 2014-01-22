d28ff0bf235abb91e6741c917351a39a33906c70
Szymon Pilkowski <szymon.pilkowski@gmail.com>
2014-01-22 16:56:36 +0100

    added fuzzy search + keyboard support

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ chrome-better-bookmark
 ======================
 
 simple chrome extension that adds a new browser button to quickly add bookmarks to the selected category (with fewer mouse clicks then using the native 'star' button)
+
+TODO
+====
+
+ - more aesthetic style
+ - keyboard shortcut
+ - icon should be greyed out by default, highlighted if the page is already bookmarked
+ - options (font size and style, focus style, key bindings, sorting options)
+ - TBD: subcategory indentation?
--- selectCategory.js
+++ selectCategory.js
@@ -1,3 +1,13 @@
+var categoryNodes = [];
+var wrapper;
+var focusedElement;
+var fuzzySearch;
+var currentNodeCount = 0;
+
+var DOWN_KEYCODE = 40;
+var UP_KEYCODE = 38;
+var CONFIRM_KEYCODE = 13;
+
 function filterRecursively(nodeArray, childrenProperty, filterFn, results) {
 
   results = results || [];
@@ -10,35 +20,6 @@ function filterRecursively(nodeArray, childrenProperty, filterFn, results) {
   return results;
 
 };
-
-chrome.bookmarks.getTree( function(t) {
-  var wrapper = document.getElementById("wrapper");
-  var categoryNodes = [];
-  var categoryUiElements = [];
-
-  categoryNodes = filterRecursively(t, "children", function(node) {
-    return !node.url && node.id > 0;
-  }).sort(function(a, b) {
-    return b.dateGroupModified - a.dateGroupModified;
-  })
-
-  categoryNodes.forEach( function( node ) {
-
-    categoryUiElements.push( createUiElement(node) );
-
-  })
-
-  categoryUiElements.forEach( function( element ) {
-    wrapper.appendChild( element );
-  })
-
-
-  wrapper.addEventListener("click", function(e) {
-    triggerClick(e.target);
-  })
-
-
-})
 
 function createUiElement(node) {
 
@@ -53,6 +34,7 @@ function createUiElement(node) {
 }
 
 function triggerClick(element) {
+
   var categoryId = element.getAttribute("data-id");
   getCurrentUrlData(function(url, title) {
 
@@ -73,7 +55,116 @@ function addBookmarkToCategory(categoryId, title, url) {
 }
 
 function getCurrentUrlData(callbackFn) {
+
   chrome.tabs.query({'active': true}, function (tabs) {
     callbackFn(tabs[0].url, tabs[0].title)
   });
+
 }
+
+function createUiFromNodes( categoryNodes ) {
+
+  var categoryUiElements = [];
+  currentNodeCount = categoryNodes.length;
+
+  categoryNodes.forEach( function( node ) {
+    categoryUiElements.push( createUiElement(node) );
+  })
+
+  categoryUiElements.forEach( function( element ) {
+    wrapper.appendChild( element );
+  });
+
+};
+
+function resetUi() {
+
+  wrapper.innerHTML = "";
+
+};
+
+function focusItem(index) {
+
+  if (focusedElement) focusedElement.classList.remove("focus");
+  focusedElement = wrapper.childNodes[index];
+  focusedElement.classList.add("focus");
+
+}
+
+function createInitialTree() {
+
+  chrome.bookmarks.getTree( function(t) {
+
+    wrapper = document.getElementById("wrapper");
+
+    var options = {
+      keys: ['title'],
+      threshold: 0.4
+    }
+    
+    categoryNodes = filterRecursively(t, "children", function(node) {
+      return !node.url && node.id > 0;
+    }).sort(function(a, b) {
+      return b.dateGroupModified - a.dateGroupModified;
+    })
+
+    createUiFromNodes( categoryNodes );
+    if (currentNodeCount > 0) focusItem(0);
+
+    fuzzySearch = new Fuse(categoryNodes, options);
+
+    wrapper.addEventListener("click", function(e) {
+      triggerClick(e.target);
+    })
+
+  });
+
+}
+
+(function() {
+
+  var searchElement = document.getElementById("search");
+  var text = "";
+  var newNodes;
+  var index = 0;
+
+  createInitialTree();
+
+  searchElement.addEventListener("keydown", function(e) {
+
+    if (e.keyCode == UP_KEYCODE) {
+      e.preventDefault();
+      index = index - 1;
+      if (index < 0) index = currentNodeCount - 1;
+      focusItem(index);
+
+    } else if (e.keyCode == DOWN_KEYCODE) {
+      e.preventDefault();
+      index = index + 1;
+      if (index >= currentNodeCount) index = 0;
+      focusItem(index);
+
+    } else if (e.keyCode == CONFIRM_KEYCODE) {
+      if (currentNodeCount > 0) triggerClick(focusedElement);
+    
+    } else {
+      // to get updated input value, we need to schedule it to the next tick
+      setTimeout( function() {
+        text = document.getElementById("search").value;
+        if (text.length) {
+          newNodes = fuzzySearch.search(text);
+          resetUi(); 
+          createUiFromNodes(newNodes) 
+          if (newNodes.length) focusItem(0);
+        } else {
+          resetUi();
+          createUiFromNodes(categoryNodes);
+        }
+      }, 0);
+    }
+
+  })
+
+  searchElement.focus();
+
+})();
